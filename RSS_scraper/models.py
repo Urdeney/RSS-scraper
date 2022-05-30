ca7d72f390e13154e597df25d62da956d4f46c0d
Andrey <andrewnureev142@gmail.com>
2022-05-30 21:46:21 +0300

    Added article model

--- RSS_scraper/models.py
+++ RSS_scraper/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from abc import ABC
 
 # Create your models here.
+
+
+class Article(ABC):
+    date: models.DateTimeField
+    link: models.CharField
+    title: models.CharField
+    description: models.TextField
+
+
+class SfeduArticle(models.Model, Article):
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/')
+    link = models.CharField(null=True)
+    title = models.CharField(null=True)
+    description = models.TextField(null=True)
+
