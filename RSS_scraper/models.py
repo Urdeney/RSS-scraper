1794ed543884446069bfa0626fc2a527a03c048d
Andrey <andrewnureev142@gmail.com>
2022-05-30 21:54:08 +0300

    Updated models and created migrations

--- RSS_scraper/models.py
+++ RSS_scraper/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from abc import ABC
 
 # Create your models here.
 
 
-class Article(ABC):
+class Article:
     date: models.DateTimeField
     link: models.CharField
     title: models.CharField
@@ -13,8 +12,8 @@ class Article(ABC):
 
 class SfeduArticle(models.Model, Article):
     date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='images/')
-    link = models.CharField(null=True)
-    title = models.CharField(null=True)
+    imageurl = models.CharField(null=True, max_length=100)
+    link = models.CharField(null=True, max_length=100)
+    title = models.CharField(null=True, max_length=100)
     description = models.TextField(null=True)
 
