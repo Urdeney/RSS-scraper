d1d19d76f22041b9acc48a192d3a987ddeb66a4b
Andrey <andrewnureev142@gmail.com>
2022-05-31 17:52:43 +0300

    Updated models and created migrations

--- RSS_scraper/models.py
+++ RSS_scraper/models.py
@@ -1,19 +1,23 @@
+from hashlib import md5
+from computedfields.models import ComputedFieldsModel, computed
 from django.db import models
 
 # Create your models here.
 
 
-class Article:
-    date: models.DateTimeField
-    link: models.CharField
-    title: models.CharField
-    description: models.TextField
+class Article(ComputedFieldsModel):
+    link: str = models.CharField(max_length=100)
+    title: str = models.CharField(max_length=100)
+    description: str = models.TextField(null=True)
+
+    @computed(models.CharField(max_length=32, unique=True), depends=[('self', ['title'])])
+    def hash(self) -> str:
+        return md5(self.title.encode('utf8')).hexdigest()
+
+    class Meta:
+        abstract = True
 
 
-class SfeduArticle(models.Model, Article):
+class SfeduArticle(Article):
     date = models.DateTimeField(auto_now_add=True)
-    imageurl = models.CharField(null=True, max_length=100)
-    link = models.CharField(null=True, max_length=100)
-    title = models.CharField(null=True, max_length=100)
-    description = models.TextField(null=True)
-
+    imageurl: str = models.CharField(max_length=100)
