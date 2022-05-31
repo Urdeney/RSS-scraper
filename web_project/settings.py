302303636e6b73b720b44d065e6463de8ec3a3b2
Andrey <andrewnureev142@gmail.com>
2022-05-31 17:48:12 +0300

    Added dependencies

--- web_project/settings.py
+++ web_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'RSS_scraper.apps.RssScraperConfig',
+    'computedfields',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
