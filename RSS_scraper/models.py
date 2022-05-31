8d1c92183ed35ac2ac644c3439bb674936db8d15
Andrey <andrewnureev142@gmail.com>
2022-05-31 19:10:38 +0300

    Add xml method

--- RSS_scraper/models.py
+++ RSS_scraper/models.py
@@ -1,4 +1,6 @@
 from hashlib import md5
+from datetime import datetime
+from abc import abstractmethod
 from computedfields.models import ComputedFieldsModel, computed
 from django.db import models
 from django.db.utils import IntegrityError
@@ -19,12 +21,16 @@ class Article(ComputedFieldsModel):
     def __str__(self) -> str:
         return f"'{self.title}' ({self.id})"
     
+    @abstractmethod
+    def to_xml(self) -> str:
+        ...
+    
     class Meta:
         abstract = True
 
 
 class SfeduArticle(Article):
-    date = models.DateTimeField(auto_now_add=True)
+    date: datetime = models.DateTimeField(auto_now_add=True)
     imageurl: str = models.CharField(max_length=100)
 
     def save(self, *args, **kwargs):
@@ -32,4 +38,14 @@ class SfeduArticle(Article):
             super().save(*args, **kwargs)
         except IntegrityError:
             pass
-
+    
+    def to_xml(self) -> str:
+        return f"""<item>
+<title>{self.title}</title>
+<link>https://sfedu.ru{self.link}</link>
+<description>{self.description}</description>
+<pubDate>{self.date.strftime(r'%a, %d %b %Y %H:%M:%S %Z')}</pubDate>
+<image>
+<url>sfedu.ru{self.imageurl}</url>
+</image>
+</item>"""
